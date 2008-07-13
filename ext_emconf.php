135046bcc630841f8eff04ebf903094e9ef38fc1
ghermens <ghermens@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-13 11:59:25 +0000

    initial upload to svn
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/gh_randomcontent/trunk@10559 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "gh_randomcontent"
+#
+# Auto generated 13-07-2008 12:14
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'GH Random Content',
+	'description' => 'This frontend plugin shows a random content element from selected page(s). It is based on onet_randomcontent, but provides more flexibility.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.2.0',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Gregor Hermens',
+	'author_email' => 'gregor.hermens@a-mazing.de',
+	'author_company' => '@mazing',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '3.5.0-0.0.0',
+			'php' => '3.0.0-0.0.0',
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"faa6";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"f301";s:17:"ext_localconf.php";s:4:"4026";s:14:"ext_tables.php";s:4:"7ceb";s:15:"flexform_ds.xml";s:4:"d6a7";s:24:"gh_randomcontent.session";s:4:"ef6d";s:23:"gh_randomcontent.webprj";s:4:"f153";s:13:"locallang.xml";s:4:"e273";s:16:"locallang_db.xml";s:4:"fd98";s:14:"pi1/ce_wiz.gif";s:4:"ac64";s:36:"pi1/class.tx_ghrandomcontent_pi1.php";s:4:"214d";s:44:"pi1/class.tx_ghrandomcontent_pi1_wizicon.php";s:4:"8bc8";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"ba12";s:14:"doc/manual.sxw";s:4:"1fa8";s:19:"doc/wizard_form.dat";s:4:"ca9a";s:20:"doc/wizard_form.html";s:4:"c2f6";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ pi1/class.tx_ghrandomcontent_pi1_wizicon.php
@@ -0,0 +1,60 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2008 Gregor Hermens (gregor.hermens@a-mazing.de)
+*  based on onet_randomcontent (c) 2005 Semyon Vyskubov (poizon@onet.ru)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * Class that adds the wizard icon.
+ *
+ * @author	Gregor Hermens <gregor.hermens@a-mazing.de>
+ */
+
+
+
+class tx_ghrandomcontent_pi1_wizicon {
+	function proc($wizardItems)	{
+		global $LANG;
+
+		$LL = $this->includeLocalLang();
+
+		$wizardItems['plugins_tx_ghrandomcontent_pi1'] = array(
+			'icon'=>t3lib_extMgm::extRelPath('gh_randomcontent').'pi1/ce_wiz.gif',
+			'title'=>$LANG->getLLL('pi1_title',$LL),
+			'description'=>$LANG->getLLL('pi1_plus_wiz_description',$LL),
+			'params'=>'&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=gh_randomcontent_pi1'
+		);
+
+		return $wizardItems;
+	}
+	function includeLocalLang()	{
+		include(t3lib_extMgm::extPath('gh_randomcontent').'locallang.php');
+		return $LOCAL_LANG;
+	}
+}
+
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/gh_randomcontent/pi1/class.tx_ghrandomcontent_pi1_wizicon.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/gh_randomcontent/pi1/class.tx_ghrandomcontent_pi1_wizicon.php']);
+}
+
+?>
