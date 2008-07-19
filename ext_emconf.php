8c529449846466ec759d0eda2145cfeb67a8db38
ghermens <ghermens@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-19 12:51:28 +0000

    Nicer ext_icon.gif, some cleanup
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/gh_randomcontent/trunk@10634 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "gh_randomcontent"
 #
-# Auto generated 13-07-2008 12:14
+# Auto generated 19-07-2008 14:35
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"faa6";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"f301";s:17:"ext_localconf.php";s:4:"4026";s:14:"ext_tables.php";s:4:"7ceb";s:15:"flexform_ds.xml";s:4:"d6a7";s:24:"gh_randomcontent.session";s:4:"ef6d";s:23:"gh_randomcontent.webprj";s:4:"f153";s:13:"locallang.xml";s:4:"e273";s:16:"locallang_db.xml";s:4:"fd98";s:14:"pi1/ce_wiz.gif";s:4:"ac64";s:36:"pi1/class.tx_ghrandomcontent_pi1.php";s:4:"214d";s:44:"pi1/class.tx_ghrandomcontent_pi1_wizicon.php";s:4:"8bc8";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"ba12";s:14:"doc/manual.sxw";s:4:"1fa8";s:19:"doc/wizard_form.dat";s:4:"ca9a";s:20:"doc/wizard_form.html";s:4:"c2f6";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"59bc";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"d4fb";s:17:"ext_localconf.php";s:4:"4026";s:14:"ext_tables.php";s:4:"7ceb";s:15:"flexform_ds.xml";s:4:"d6a7";s:13:"locallang.xml";s:4:"e273";s:16:"locallang_db.xml";s:4:"fd98";s:14:"pi1/ce_wiz.gif";s:4:"ac64";s:36:"pi1/class.tx_ghrandomcontent_pi1.php";s:4:"214d";s:44:"pi1/class.tx_ghrandomcontent_pi1_wizicon.php";s:4:"8bc8";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"ba12";s:14:"doc/manual.sxw";s:4:"1fa8";s:19:"doc/wizard_form.dat";s:4:"ca9a";s:20:"doc/wizard_form.html";s:4:"c2f6";}',
 	'suggests' => array(
 	),
 );
