b1583ad1db8dc71f7314fbcaf3280df392ef7e81
ghermens <ghermens@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-05 15:42:53 +0000

    * Some CGL cleanups
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/gh_randomcontent/trunk@60413 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_ghrandomcontent_pi1.php
+++ pi1/class.tx_ghrandomcontent_pi1.php
@@ -1,45 +1,45 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2008-2010 Gregor Hermens (gregor.hermens@a-mazing.de)
-*  based on onet_randomcontent (c) 2005 Semyon Vyskubov (poizon@onet.ru)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2008-2010 Gregor Hermens (gregor.hermens@a-mazing.de)
+ *  based on onet_randomcontent (c) 2005 Semyon Vyskubov (poizon@onet.ru)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 /**
  * [CLASS/FUNCTION INDEX of SCRIPT]
  *
  *
  *
  *   52: class tx_ghrandomcontent_pi1 extends tslib_pibase
- *   65:     public function main($content,$conf)
- *   92:     protected function init($conf)
- *  126:     protected function getContentUids()
- *  155:     protected function selectContentUIDs ($content_ids = array())
- *  173:     protected function renderContent($content_shown = array(), $content_ids = array())
+ *   65:	 public function main($content, $conf)
+ *   92:	 protected function init($conf)
+ *  126:	 protected function getContentUids()
+ *  155:	 protected function selectContentUids ($contentIds = array())
+ *  173:	 protected function renderContent($contentShown = array(), $contentIds = array())
  *
  * TOTAL FUNCTIONS: 5
  * (This index is automatically created/updated by the extension "extdeveval")
  *
  */
 
-require_once(PATH_tslib.'class.tslib_pibase.php');
+require_once(PATH_tslib . 'class.tslib_pibase.php');
 
 
 /**
@@ -49,35 +49,60 @@ require_once(PATH_tslib.'class.tslib_pibase.php');
  * @package	TYPO3
  * @subpackage	tx_ghrandomcontent
  */
-class tx_ghrandomcontent_pi1 extends tslib_pibase {
-	public $prefixId = 'tx_ghrandomcontent_pi1';		// Same as class name
-	public $scriptRelPath = 'pi1/class.tx_ghrandomcontent_pi1.php';	// Path to this script relative to the extension dir.
-	public $extKey = 'gh_randomcontent';	// The extension key.
+class tx_ghrandomcontent_pi1 extends tslib_pibase
+{
+	/**
+	 * Same as class name
+	 *
+	 * @var string
+	 */
+	public $prefixId = 'tx_ghrandomcontent_pi1';
+
+	/**
+	 * Path to this script relative to the extension dir.
+	 *
+	 * @var string
+	 */
+	public $scriptRelPath = 'pi1/class.tx_ghrandomcontent_pi1.php';
+
+	/**
+	 * The extension key
+	 *
+	 * @var string
+	 */
+	public $extKey = 'gh_randomcontent';
+
+	/**
+	 * Check the CHash parameter
+	 *
+	 * @var bool
+	 */
 	public $pi_checkCHash = TRUE;
 
 	/**
 	 * The main method of the PlugIn
 	 *
-	 * @param	string		$content: The PlugIn content
-	 * @param	array		$conf: The PlugIn configuration
-	 * @return	string		The content that is displayed on the website
+	 * @param   string      $content The PlugIn content
+	 * @param   array       $conf The PlugIn configuration
+	 * @return  string      The content that is displayed on the website
 	 */
-	public function main($content,$conf)	{
+	public function main($content, $conf) {
 		$this->init($conf);
 
-		$content_ids = $this->getContentUids();
+		$contentIds = $this->getContentUids();
 
-		if(!count($content_ids)) { // no content available at all
-			return false;
+			// no content available at all
+		if (!count($contentIds)) {
+			return FALSE;
 		}
 
-		if($this->conf['count'] > count($content_ids)) {
-			$this->conf['count'] = count($content_ids);
+		if ($this->conf['count'] > count($contentIds)) {
+			$this->conf['count'] = count($contentIds);
 		}
 
-		$content_shown = $this->selectContentUIDs($content_ids);
+		$contentShown = $this->selectContentUids($contentIds);
 
-		$content = $this->renderContent($content_shown, $content_ids);
+		$content = $this->renderContent($contentShown, $contentIds);
 
 		return $content;
 	}
@@ -86,36 +111,39 @@ class tx_ghrandomcontent_pi1 extends tslib_pibase {
 	/**
 	 * Initialise this class
 	 *
-	 * @param	array		$conf: The PlugIn configuration
-	 * @return	boolean		success
+	 * @param   array       $conf The PlugIn configuration
+	 * @return  boolean     success
 	 */
 	protected function init($conf) {
 		$this->conf = $conf;
-		$this->pi_initPIflexForm();		// Init FlexForm configuration for plugin
-		if($this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'which_pages', 'sDEF')) {
+
+			// Init FlexForm configuration for plugin
+		$this->pi_initPIflexForm();
+		if ($this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'which_pages', 'sDEF')) {
 			$this->conf['pages'] = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'which_pages', 'sDEF');
 		}
 
-		$this->conf['count'] = (int) $this->conf['count'];
-		if($this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'count', 'sDEF')) {
-			$this->conf['count'] = (int) $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'count', 'sDEF');
+		$this->conf['count'] = (int)$this->conf['count'];
+		if ($this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'count', 'sDEF')) {
+			$this->conf['count'] = (int)$this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'count', 'sDEF');
 		}
-		if(empty($this->conf['count'])) {
+		if (empty($this->conf['count'])) {
 			$this->conf['count'] = 1;
 		}
 
-		if($this->cObj->data['list_type'] == $this->extKey . '_pi1') { // Override $conf with flexform checkboxes
+			// Override $conf with flexform checkboxes
+		if ($this->cObj->data['list_type'] == $this->extKey . '_pi1') {
 			$this->conf['honorLanguage'] = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'honor_language', 'sDEF');
 			$this->conf['honorColPos'] = $this->pi_getFFvalue($this->cObj->data['pi_flexform'], 'honor_colpos', 'sDEF');
 		}
 
-		if('' == $this->cObj->data['colPos']) {
+		if ('' == $this->cObj->data['colPos']) {
 			$this->conf['colPos'] = $this->conf['defaultColPos'];
 		} else {
 			$this->conf['colPos'] = $this->cObj->data['colPos'];
 		}
 
-		return true;
+		return TRUE;
 	}
 
 	/**
@@ -126,14 +154,14 @@ class tx_ghrandomcontent_pi1 extends tslib_pibase {
 	protected function getContentUids() {
 		$where = 'pid IN(' . $this->conf['pages'] . ' ) ' . $this->cObj->enableFields('tt_content');
 
-		if($this->conf['honorLanguage']) {
+		if ($this->conf['honorLanguage']) {
 			$where .= ' AND ( sys_language_uid = ' . $GLOBALS['TSFE']->sys_page->sys_language_uid . ' OR sys_language_uid = -1 )';
 		}
-		if($this->conf['honorColPos']) {
+		if ($this->conf['honorColPos']) {
 			$where .= ' AND colPos = ' . $this->conf['colPos'];
 		}
 
-		$content_ids = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
+		$contentIds = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
 			'uid,pid',
 			'tt_content',
 			$where,
@@ -143,61 +171,62 @@ class tx_ghrandomcontent_pi1 extends tslib_pibase {
 			'uid'
 		);
 
-		return $content_ids;
+		return $contentIds;
 	}
 
 	/**
 	 * Select the content elements to be shown by random
 	 *
-	 * @param	array		List of content element UIDs and their PIDs to select from
-	 * @return	array		List of content element UIDs and their PIDs
+	 * @param   array    $contentIds List of content element UIDs and their PIDs to select from
+	 * @return  array    List of content element UIDs and their PIDs
 	 */
-	protected function selectContentUIDs ($content_ids = array()) {
-		$content_shown = array_rand($content_ids, $this->conf['count']); // choose random content element
-		if(1 == $this->conf['count']) {
-			$content_shown = array($content_shown);
+	protected function selectContentUids($contentIds = array()) {
+			// choose random content elements
+		$contentShown = array_rand($contentIds, $this->conf['count']);
+		if (1 == $this->conf['count']) {
+			$contentShown = array($contentShown);
 		} else {
-			shuffle($content_shown);
+			shuffle($contentShown);
 		}
 
-		return $content_shown;
+		return $contentShown;
 	}
 
 	/**
 	 * Render selected content elements
 	 *
-	 * @param	array		List of content element UIDs to show
-	 * @param	array		List of all available content element UIDs and their PIDs
-	 * @return	string		HTML
+	 * @param   array       $contentShown List of content element UIDs to show
+	 * @param   array       $contentIds List of all available content element UIDs and their PIDs
+	 * @return  string      HTML
 	 */
-	protected function renderContent($content_shown = array(), $content_ids = array()) {
+	protected function renderContent($contentShown = array(), $contentIds = array()) {
 		$content = '';
-		foreach($content_shown as $content_uid) {
-			// render content element
-			$content_conf = array(
+		foreach ($contentShown as $contentUid) {
+				// render content element
+			$contentConf = array(
 				'table' => 'tt_content',
 				'select.' => array(
-					'uidInList' => $content_ids[$content_uid]['uid'],
-					'pidInList' => $content_ids[$content_uid]['pid'],
+					'uidInList' => $contentIds[$contentUid]['uid'],
+					'pidInList' => $contentIds[$contentUid]['pid'],
 				),
 			);
 
-			$element = $this->cObj->CONTENT($content_conf);
+			$element = $this->cObj->CONTENT($contentConf);
 
-			if(!empty($this->conf['elementWrap.'])) {
+			if (!empty($this->conf['elementWrap.'])) {
 				$element = $this->cObj->stdWrap($element, $this->conf['elementWrap.']);
 			}
-			if(!empty($this->conf['elementWrap'])) {
+			if (!empty($this->conf['elementWrap'])) {
 				$element = $this->cObj->wrap($element, $this->conf['elementWrap']);
 			}
 
 			$content .= $element;
 		}
 
-		if(!empty($this->conf['allWrap.'])) {
+		if (!empty($this->conf['allWrap.'])) {
 			$content = $this->cObj->stdWrap($content, $this->conf['allWrap.']);
 		}
-		if(!empty($this->conf['allWrap'])) {
+		if (!empty($this->conf['allWrap'])) {
 			$content = $this->cObj->wrap($content, $this->conf['allWrap']);
 		}
 
@@ -205,7 +234,7 @@ class tx_ghrandomcontent_pi1 extends tslib_pibase {
 	}
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/gh_randomcontent/pi1/class.tx_ghrandomcontent_pi1.php'])	{
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/gh_randomcontent/pi1/class.tx_ghrandomcontent_pi1.php']) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/gh_randomcontent/pi1/class.tx_ghrandomcontent_pi1.php']);
 }
 ?>
--- pi1/class.tx_ghrandomcontent_pi1_wizicon.php
+++ pi1/class.tx_ghrandomcontent_pi1_wizicon.php
@@ -1,56 +1,63 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2008 Gregor Hermens (gregor.hermens@a-mazing.de)
-*  based on onet_randomcontent (c) 2005 Semyon Vyskubov (poizon@onet.ru)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2008 Gregor Hermens (gregor.hermens@a-mazing.de)
+ *  based on onet_randomcontent (c) 2005 Semyon Vyskubov (poizon@onet.ru)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
 /**
  * Class that adds the wizard icon.
  *
  * @author	Gregor Hermens <gregor.hermens@a-mazing.de>
+ * @package	TYPO3
+ * @subpackage	tx_ghrandomcontent
  */
+class tx_ghrandomcontent_pi1_wizicon
+{
 
+	/**
+	 * Insert plugin configuration into $izardItems array
+	 *
+	 * @param array $wizardItems
+	 * @return array
+	 */
+	public function proc($wizardItems) {
 
-
-class tx_ghrandomcontent_pi1_wizicon {
-	function proc($wizardItems)	{
-		global $LANG;
-
-		$LL = t3lib_div::readLLfile(t3lib_extMgm::extPath('gh_randomcontent').'locallang.xml', $LANG->lang, $LANG->charSet);
+		$localLang = t3lib_div::readLLfile(
+			t3lib_extMgm::extPath('gh_randomcontent') . 'locallang.xml',
+			$GLOBALS['LANG']->lang, $GLOBALS['LANG']->charSet
+		);
 
 		$wizardItems['plugins_tx_ghrandomcontent_pi1'] = array(
-			'icon'=>t3lib_extMgm::extRelPath('gh_randomcontent').'pi1/ce_wiz.gif',
-			'title'=>$LANG->getLLL('pi1_title',$LL),
-			'description'=>$LANG->getLLL('pi1_plus_wiz_description',$LL),
-			'params'=>'&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=gh_randomcontent_pi1'
+			'icon' => t3lib_extMgm::extRelPath('gh_randomcontent') . 'pi1/ce_wiz.gif',
+			'title' => $GLOBALS['LANG']->getLLL('pi1_title', $localLang),
+			'description' => $GLOBALS['LANG']->getLLL('pi1_plus_wiz_description', $localLang),
+			'params' => '&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=gh_randomcontent_pi1'
 		);
 
 		return $wizardItems;
 	}
 }
 
-
-
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/gh_randomcontent/pi1/class.tx_ghrandomcontent_pi1_wizicon.php'])	{
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/gh_randomcontent/pi1/class.tx_ghrandomcontent_pi1_wizicon.php']) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/gh_randomcontent/pi1/class.tx_ghrandomcontent_pi1_wizicon.php']);
 }
-
 ?>
